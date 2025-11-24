a585afc8ce35d6788f48786b0108755a7b388e43
Luis <hkf24kdns@gmail.com>
2025-11-24 17:09:02 +0100

    Adds basic HomeActivity and WelcomeFragment
    Implements a basic HomeActivity to serve as the main screen after login.
    Adds a WelcomeFragment to display a welcome message and a "Get Started" button.
    The "Get Started" button is currently non-functional and will be implemented later.
    The HomeActivity uses a FrameLayout to host the WelcomeFragment.
    Includes the required layout files and drawables.

--- app/src/main/java/com/example/appprenotame/activities/HomeActivity.java
+++ app/src/main/java/com/example/appprenotame/activities/HomeActivity.java
@@ -1,9 +1,6 @@
 package com.example.appprenotame.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,30 +9,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.appprenotame.R;
+import com.example.appprenotame.fragments.WelcomeFragment;
 
-public class LoginActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        TextView SignUpButton = findViewById(R.id.textSpan_SignUp);
-        SignUpButton.setOnClickListener(v -> {
-            Intent intent = new Intent(LoginActivity.this , RegisterActivity.class);
-            startActivity(intent);
-        });
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container_view, new WelcomeFragment())
+                    .commit();
+        }
 
-        Button buttonLogin = findViewById(R.id.buttonLogin);
-        buttonLogin.setOnClickListener(v -> {
-            Intent intent = new Intent(LoginActivity.this , HomeActivity.class);
-            startActivity(intent);
-        });
     }
+
+
 }
