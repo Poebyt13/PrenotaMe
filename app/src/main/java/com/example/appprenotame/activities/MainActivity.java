9c26783b139c75ef527e88431d4674bb80275bdc
Moaaz980 <moaaz.bahnassawi@itsrizzoli.it>
2025-11-24 14:37:28 +0100

    Home page finita

--- app/src/main/java/com/example/appprenotame/activities/MainActivity.java
+++ app/src/main/java/com/example/appprenotame/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.appprenotame.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,11 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView button = findViewById(R.id.button_text);
+         button.setOnClickListener(v -> {
+             Intent intent = new Intent(MainActivity.this , LoginActivity.class);
+             startActivity(intent);
+         });
+
+
+
+
     }
 }
