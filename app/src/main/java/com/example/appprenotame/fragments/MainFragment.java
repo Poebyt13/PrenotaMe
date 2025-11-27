79b90be051faa06d23d83558b2b74db05814422e
Luis <hkf24kdns@gmail.com>
2025-11-27 16:35:30 +0100

    Implements basic home screen layout
    Sets up the basic layout for the home screen, including a title, search bar, and a sample event display.
    
    It introduces conditional rendering based on admin status, displaying different title variations.
    Also adds a number of drawable resources to compose the ui components.

--- app/src/main/java/com/example/appprenotame/fragments/MainFragment.java
+++ app/src/main/java/com/example/appprenotame/fragments/MainFragment.java
@@ -2,13 +2,19 @@ package com.example.appprenotame.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.appprenotame.R;
+
+import org.w3c.dom.Text;
 
 public class MainFragment extends Fragment {
 
@@ -17,9 +23,26 @@ public class MainFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        boolean isAdmin = false; // Dovremmo determinare questo valore in base allo stato di accesso dell'utente
+
+        TextView titolo = view.findViewById(R.id.textViewTitleHome);
+        LinearLayout titoloWithAdminOptions = view.findViewById(R.id.linearLayoutTitleHome);
+
+        if (isAdmin) {
+            titoloWithAdminOptions.setVisibility(View.VISIBLE);
+        } else {
+            titolo.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
